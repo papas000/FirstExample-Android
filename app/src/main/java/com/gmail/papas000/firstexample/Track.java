deca56469320d53f545fb18ab1081121b45d1e0b
papas000 <papas000@gmail.com>
2020-08-01 04:47:06 +0200

    init

---
+++ app/src/main/java/com/gmail/papas000/firstexample/Track.java
@@ -0,0 +1,11 @@
+package com.gmail.papas000.firstexample;
+
+public class Track {
+    public String name;
+    public String instructor;
+
+    Track(String name, String instructor) {
+        this.name = name;
+        this.instructor = instructor;
+    }
+}
